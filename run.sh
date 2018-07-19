b7c967fde4360d28bb29c8b50ca4ba2739ee2edd
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2018-07-18 22:25:58 -0500

    Permite ejecutar el proceso en background.

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-virtualenv --no-site-packages --distribute .env && source .env/bin/activate && pip install -r requirements.txt
+virtualenv -p python3 --no-site-packages --distribute .env && source .env/bin/activate && pip install -r requirements.txt
+
+cd src/ && nohup python bot.py > log.txt 2>&1 &
