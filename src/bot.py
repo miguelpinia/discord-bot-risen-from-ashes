fdc6c2ba30d1f8df7c3ca034139cfac51352d3a4
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2018-07-18 20:58:25 -0500

    Simplifica código.

--- src/bot.py
+++ src/bot.py
@@ -10,29 +10,15 @@ from utils import get_token, process_data, generate_image_es, generate_image_en
 
 TOKEN = get_token(expanduser('~/.discord_token'))
 URL = "https://fallin-angels.org/servers.php?serverid=37"
+
+TEMPLATE_ES = 'Mapa:    \t\t\t\t\t{}\nLimite Banderas:  \t{}\nBanderas\
+ Rojos:\t\t{}\nBanderas Azules:\t {}\nTiempo del mapa:\t{}\nSiguiente\
+ Mapa:\t\t{}'
+
+TEMPLATE_EN = 'Map:\t\t\t\t{}\nFlags Limit:\t{}\nRed  Flags:\t\t{}\nBlue\
+ Flags:\t\t{}\nMap Time:\t{}\nNext Map:\t{}'
+
 client = discord.Client()
-
-
-def mapa_esp(datos):
-    """Get data about the map in Spanish."""
-    return 'Mapa:    \t\t\t\t\t{}\nLimite Banderas:  \t{}\nBanderas Rojos:\t\t{}\nBanderas \
-Azules:\t {}\nTiempo del mapa:\t{}\nSiguiente Mapa:\t\t{}'.format(datos['mapa'],
-                                                                  datos['banderas'],
-                                                                  datos['rojas'],
-                                                                  datos['azules'],
-                                                                  datos['tiempo'],
-                                                                  datos['next_map'])
-
-
-def map_eng(data):
-    """Get data about the map in English."""
-    return 'Map:\t\t\t\t{}\nFlags Limit:\t{}\nRed  Flags:\t\t{}\nBlue Flags:\t\t{}\n\
-Map Time:\t{}\nNext Map:\t{}'.format(data['mapa'],
-                                     data['banderas'],
-                                     data['rojas'],
-                                     data['azules'],
-                                     data['tiempo'],
-                                     data['next_map'])
 
 
 def next_map():
@@ -46,14 +32,26 @@ def info_es():
     """Information about the current map in Spanish."""
     file_info = request.urlopen(URL)
     data = file_info.read().decode('utf-8')
-    return mapa_esp(process_data(data))
+    data = process_data(data)
+    return TEMPLATE_ES.format(data['mapa'],
+                              data['banderas'],
+                              data['rojas'],
+                              data['azules'],
+                              data['tiempo'],
+                              data['next_map'])
 
 
 def info_en():
     """Information about the current map in english."""
     file_info = request.urlopen(URL)
     data = file_info.read().decode('utf-8')
-    return map_eng(process_data(data))
+    data = process_data(data)
+    return TEMPLATE_EN.format(data['mapa'],
+                              data['banderas'],
+                              data['rojas'],
+                              data['azules'],
+                              data['tiempo'],
+                              data['next_map'])
 
 
 def img_es():
@@ -107,6 +105,16 @@ async def on_message(message):
     if msg is None:
         return
     await client.send_message(message.channel, msg)
+
+
+@client.event
+async def on_member_join(member):
+    """
+    Display a message when a new member join to the server.
+    """
+    server = member.server
+    fmt = 'Welcome {0.mention} to {1.name}!'
+    await client.send_message(server, fmt.format(member, server))
 
 
 @client.event
